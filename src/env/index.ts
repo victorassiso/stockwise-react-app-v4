2e7c6d0b70b2604b3c651fb2d455c5a0b51c5b2a
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-12 23:54:11 -0300

    ADD: .env

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_MODULE: z
+    .enum(['development', 'test', 'production'])
+    .default('production'),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables!', _env.error.format())
+
+  throw new Error('Invalid environment variables!')
+}
+
+export const env = _env.data
