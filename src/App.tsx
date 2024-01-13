c41346cfe34a37ff57bcd3401ef88b08bf0d955c
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-12 22:23:28 -0300

    ADD: Tailwind and shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import './global.css'
+
 export function App() {
   return (
     <div>
-      <h1>App</h1>
+      <h1 className="text-4xl text-green-500">App</h1>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { Package } from 'phosphor-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
